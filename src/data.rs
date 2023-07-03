aab8c66bffb36a68fb0deb3f97d1e5e4ed67cf39
arne-fuchs <arnefuchs@protonmail.com>
2023-07-03 11:29:33 +0200

    Added more information for commodities

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,9 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::env::var;
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use json::JsonValue;
 use rocket::fairing::AdHoc;
 use rocket::State;
 use rocket::serde::{Serialize, Deserialize, json::Json};
@@ -12,16 +14,17 @@ use rocket_sync_db_pools::database;
 use rusqlite::params;
 
 use rocket_sync_db_pools::rusqlite;
+use serde_json::{json, Value};
 
 #[database("rusqlite")]
 struct Db(rusqlite::Connection);
 
 struct Cache {
-    commodity: HashMap<String, CommodityCache>
+    commodity: HashMap<String, CommodityCache>,
 }
 
 impl Cache {
-    fn get_commodity(&self, name: String) -> Option<Commodity>{
+    fn get_commodity(&self, name: String) -> Option<Commodity> {
         let result = self.commodity.get(name.as_str());
         return match result {
             None => {
@@ -38,15 +41,15 @@ impl Cache {
                     Some(commodity.data.clone())
                 }
             }
-        }
+        };
     }
 
-    fn put_commodity(&mut self, commodity: Commodity, name: String){
-        let commodity_cache = CommodityCache{
-            instant : Instant::now(),
-            data: commodity
+    fn put_commodity(&mut self, commodity: Commodity, name: String) {
+        let commodity_cache = CommodityCache {
+            instant: Instant::now(),
+            data: commodity,
         };
-        self.commodity.insert(name,commodity_cache);
+        self.commodity.insert(name, commodity_cache);
     }
 }
 
@@ -57,12 +60,12 @@ async fn root() -> &'static str {
 
 
 /**
-* Commodity
-**/
-struct CommodityCache {
+ * Commodity
+ **/ struct CommodityCache {
     instant: Instant,
-    data: Commodity
+    data: Commodity,
 }
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Commodity {
@@ -70,8 +73,11 @@ struct Commodity {
     name: Option<String>,
     buy_price: Option<f64>,
     sell_price: Option<f64>,
-    avg_price: Option<f64>,
+    mean_price: Option<f64>,
+    lowest_buy_price: Option<Value>,
+    highest_sell_price: Option<Value>,
 }
+
 #[get("/commodity/<name>")]
 async fn commodity(cache: &State<Arc<Mutex<Cache>>>, db: Db, name: String) -> Option<Json<Commodity>> {
     let name_clone = name.clone();
@@ -79,19 +85,66 @@ async fn commodity(cache: &State<Arc<Mutex<Cache>>>, db: Db, name: String) -> Op
     return match some_commodity {
         None => {
             let commodity: Commodity = db.run(move |conn| {
-                conn.query_row("SELECT avg(buy_price),avg(sell_price),avg(mean_price) from commodity where name = ?", params![name_clone.clone()],
-                               |r| Ok(Commodity {
-                                   name: Option::from(name_clone),
-                                   buy_price: r.get(0)?,
-                                   sell_price: r.get(1)?,
-                                   avg_price: r.get(2)?
-                               }))
+                conn.query_row("
+                select avg(buy_price),
+       avg(sell_price),
+       avg(mean_price),
+       min(case when buy_price > 0 and stock > 1000 then buy_price end) as lowest_buy_price,
+       lowest_buy_station,
+       lowest_buy_system,
+       max(sell_price) as highest_sell_price,
+       highest_sell_station,
+       highest_sell_system
+from commodity
+        left join
+     (select station_name as highest_sell_station, system_name as highest_sell_system
+      from station
+      where station.market_id = (select market_id
+                                 from commodity
+                                 where sell_price = (select max(sell_price)
+                                                     from commodity
+                                                     where name = ? limit 1)))
+                                                     left join
+     (select station_name as lowest_buy_station, system_name as lowest_buy_system
+      from station
+      where station.market_id = (select market_id
+                                 from commodity
+                                 where buy_price = (select min(case when buy_price > 0 then buy_price end)
+                                                     from commodity
+                                                     where name = ? and stock > 1000 limit 1)))
+
+where name = ?;
+", params![name_clone.clone(),name_clone.clone(),name_clone.clone()],
+                               |r| {
+                                   let lowest_buy_data = json!(
+                                       {
+                                           "buy_price": r.get::<usize,u64>(3).unwrap(),
+                                           "station": r.get::<usize,String>(4).unwrap(),
+                                           "system": r.get::<usize,String>(5).unwrap()
+                                       }
+                                   );
+                                   let highest_sell_data = json!(
+                                       {
+                                           "sell_price": r.get::<usize,u64>(6).unwrap(),
+                                           "station": r.get::<usize,String>(7).unwrap(),
+                                           "system": r.get::<usize,String>(8).unwrap()
+                                       }
+                                   );
+                                   Ok(Commodity {
+                                       name: Option::from(name_clone),
+                                       buy_price: r.get(0)?,
+                                       sell_price: r.get(1)?,
+                                       mean_price: r.get(2)?,
+                                       lowest_buy_price: Some(lowest_buy_data),
+                                       highest_sell_price: Some(highest_sell_data),
+                                   })
+                               })
             }).await.ok()?;
 
             //Check if value is there. If not, do not cache! May lead to let memory bloat if there are too many wrong api calls
-            match commodity.avg_price {
+            match commodity.mean_price {
                 None => {}
-                Some(_) => {cache.lock().unwrap().put_commodity(commodity.clone(),name.clone());}
+                Some(_) => { cache.lock().unwrap().put_commodity(commodity.clone(), name.clone()); }
             }
 
             Some(Json(commodity))
@@ -99,19 +152,17 @@ async fn commodity(cache: &State<Arc<Mutex<Cache>>>, db: Db, name: String) -> Op
         Some(commodity) => {
             Some(Json(commodity))
         }
-    }
+    };
 }
 
 pub fn stage() -> AdHoc {
-    let cache = Cache{
+    let cache = Cache {
         commodity: HashMap::new(),
     };
 
     let cache_mutex = Arc::new(Mutex::new(cache));
 
     AdHoc::on_ignite("Data Stage", |rocket| async {
-        rocket.attach(Db::fairing())
-            .manage(cache_mutex)
-            .mount("/data", routes![root,commodity])
+        rocket.attach(Db::fairing()).manage(cache_mutex).mount("/data", routes![root,commodity])
     })
 }
